77017c822887c1cf50dcf1ccf42e89d371ae706f
ShehanRandikaFdz <IT23449282@my.sliit.lk>
2025-03-19 14:33:25 +0530

    Creating app.js
    Establishing Database Connection

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,1 +1,16 @@
-console.log ("Hi Shehan Fernando");
+const express = require('express');
+const mongoose = require('mongoose'); 
+const router  = require('./Routes/StudentRoutes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use("/student" , router);
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://admin:itp25@cluster0.srq74.mongodb.net/")
+    .then(() => {console.log("MongoDB Connected");
+        app.listen(5000);
+    })
+    .catch((err) => console.log(err));
